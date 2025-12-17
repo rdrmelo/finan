6c1725f92c1fa829c7d7259ac279436e4986d5c4
Rodrigo da Rocha Melo <rodrigo.melo@pucrs.br>
2025-12-17 15:48:35 -0300

    Ajustes na autenticação

--- js/services/auth.js
+++ js/services/auth.js
@@ -1,15 +1,22 @@
-
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged,
-    sendEmailVerification
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    updateProfile,
+    updatePassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 import { auth } from "./firebaseConfig.js";
 
+// Mapped exports to match app.js imports
 export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
 export const register = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const logout = () => signOut(auth);
-export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
+export const subscribeToAuthChanges = (callback) => onAuthStateChanged(auth, callback);
 export const verifyEmail = (user) => sendEmailVerification(user);
+export const sendPasswordReset = (email) => sendPasswordResetEmail(auth, email);
+export const updateUserProfile = (user, data) => updateProfile(user, data);
+export const changeUserPassword = (user, newPassword) => updatePassword(user, newPassword);
